400d2364b5ef1142ea1f976a0bc8a8513fb0990b
Thanos Korakas <seekerx@Thanoss-MacBook-Pro.local>
2017-03-22 11:45:43 +0200

    Inital commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# SurvivorKiller Chrome extension
+
+SurvivorKiller is a Chrome extension that finds and terminates Survivor (Greek TV show) posts.
+
+## How to install
+* Download extension (Click 'Clone or download' button and then 'Download zip')
+* Unzip
+* Navigate to "chrome://extensions"
+* Enable "Developer mode" checkbox at the top right corner
+* Click "Load unpacked extension..."
+* Browse and select extension folder (not the zip file)
---
+++ script.js
@@ -0,0 +1,32 @@
+// Survivor dictionary.
+const spoilers = ['survivor', 'τσανκ', 'κινέζος', 'Ορέστης', 'αγώνισμα', 'Ορέστης', 'Σάρα', 'Τσανγκ', 'Μισθοφόρος',
+  'Λάουρα', 'Σπαλιάρας', 'Σπαλ', 'Χρανιώλα', 'Χρανιώτης', 'ΜΠΟ ', 'Ασυλία', 'Κολιδά', 'Μαχητές',
+  'Αγγελόπουλος', 'Βαλαβάνη', 'orestis', 'Ελισάβετ', 'ΒΑΛΑΒΑΝΗ'];
+
+// Generate regex string.
+const spoilersRegex = new RegExp(spoilers.join('|'), 'i');
+
+// On document ready start looking for survivor.
+$(document).ready(function () {
+  killSurvivor();
+  $(document).scroll(function () {
+    setTimeout((function () {
+      killSurvivor()
+    }), 150);
+  });
+});
+
+/**
+ * Finds and terminates posts that contain survivor thinks.
+ */
+killSurvivor = function () {
+  const newElements = $('.userContent');
+  if (newElements.length === 0) {
+    return;
+  }
+  newElements.each(function () {
+    if (this.textContent.match(spoilersRegex) !== null) {
+      $(this).closest('.userContentWrapper').replaceWith('<h1 style="border-style: solid;">Survivor found</h1>');
+    }
+  });
+};
