5995a01aeb2a219396622ba6e5b129afcb4d68b7
Thanos Korakas <thanoskorakas@gmail.com>
2018-02-03 18:29:04 +0200

    Removed jQuery and refactored code using vanilla JS

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
 // Survivor dictionary
-const spoilers = ['survivor', 'τσανκ', 'κινέζος', 'Ορέστης', 'αγώνισμα', 'Ορέστης', 'Σάρα', 'Τσανγκ', 'Μισθοφόρος',
-    'Λάουρα', 'Σπαλιάρας', 'Σπαλ', 'Χρανιώλα', 'Χρανιώτης', 'ΜΠΟ ', 'Ασυλία', 'Κολιδά', 'Μαχητές',
-    'Αγγελόπουλος', 'Βαλαβάνη', 'orestis', 'Ελισάβετ', 'ΒΑΛΑΒΑΝΗ', 'ΕΛΙΣΑΒΕΤ', 'mounopanos'
-];
+const spoilers = [
+  'survivor', 'τσανκ', 'κινέζος', 'Ορέστης', 'αγώνισμα', 'Ορέστης', 'Σάρα', 'Τσανγκ', 'Μισθοφόρος',
+  'Λάουρα', 'Σπαλιάρας', 'Σπαλ', 'Χρανιώλα', 'Χρανιώτης', 'ΜΠΟ ', 'Ασυλία', 'Κολιδά', 'Μαχητές',
+  'Αγγελόπουλος', 'Βαλαβάνη', 'orestis', 'Ελισάβετ', 'ΒΑΛΑΒΑΝΗ', 'ΕΛΙΣΑΒΕΤ', 'mounopanos'
+]
 
 // Generate regex string
-const spoilersRegex = new RegExp(spoilers.join('|'), 'i');
+const spoilersRegex = new RegExp(spoilers.join('|'), 'i')
 
-// On document ready start looking for survivor related stuff
-$(document).ready(function() {
-    killSurvivor();
-    $(document).scroll(function() {
-        setTimeout((function() {
-            killSurvivor()
-        }), 200);
-    });
-});
+function killOnScroll (e) {
+  killSurvivor()
+}
 
-// Finds and terminates posts that contain survivor things.
-killSurvivor = function() {
-    const elementsFB = ['.userContent', '.lfloat'];
-    elementsFB.forEach(function(x) {
-        if ($(x).length) {
-            $(x).each(function() {
-                if (this.textContent.match(spoilersRegex) !== null) {
-                    $(this).closest('.userContentWrapper').replaceWith('<div style="display:none;"></div>');
-                }
-            });
-        }
-    });
+function init () {
+  window.addEventListener('scroll', killOnScroll)
+}
+
+window.onload = function () {
+  init()
+  killSurvivor()
+}
+
+function closestByClass (el, classToFound) {
+  while (!el.className.includes(classToFound)) {
+    el = el.parentNode
+    if (!el || el.className === undefined) {
+      return null
+    }
+  }
+  return el
+}
+
+// Finds and terminates posts that contain survivor things
+function killSurvivor () {
+  const elementsFB = ['.userContent']
+
+  const elements = document.querySelectorAll(elementsFB)
+  elements.forEach(function (element) {
+    if (element.innerText.match(spoilersRegex) !== null && !element.classList.contains('survivor-found')) {
+      element.classList.add('survivor-found')
+      const newElement = document.createElement('span')
+      newElement.innerHTML = 'Survivor found, click to see the post! :('
+      newElement.style = 'font-size: 20px;'
+      newElement.addEventListener('click', function(e) {
+        e.target.replaceWith(element)
+      })
+      closestByClass(element, 'userContentWrapper').replaceWith(newElement)
+    }
+  })
 }
